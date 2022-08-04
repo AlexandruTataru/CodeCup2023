dcd21ef6337cbe7440572875bb8040f2e9a13d4e
Alexandru Tataru <alexandruflavian@gmail.com>
2022-08-04 21:50:42 +0300

    Added new test cases and improved competition scripts output

--- scripts/run_caiaio_competition.sh
+++ scripts/run_caiaio_competition.sh
@@ -4,12 +4,14 @@ mkdir build
 cd build
 cmake ../..
 make
+make test
 cd ..
 rm -rf ./entropy/refereelogs/*
 rm -rf ./entropy/playerlogs/*
 cd entropy/bin
 cp ../../build/src/bot .
 ./caiaio
-./competition.sh bot player1 player2 player3
+./competition.sh bot player1 player2 player3 > competition.out
+tail -4 competition.out
 cd ../..
 rm -rf ./build
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,7 +2,46 @@
 #include <catch2/catch.hpp>
 #include <lib.hpp>
 
-TEST_CASE("Quick check", "[defines]")
+TEST_CASE("Rows to letters", "[defines]")
 {
     REQUIRE(ROW_TO_LETTER(0) == 'A');
+    REQUIRE(ROW_TO_LETTER(1) == 'B');
+    REQUIRE(ROW_TO_LETTER(2) == 'C');
+    REQUIRE(ROW_TO_LETTER(3) == 'D');
+    REQUIRE(ROW_TO_LETTER(4) == 'E');
+    REQUIRE(ROW_TO_LETTER(5) == 'F');
+    REQUIRE(ROW_TO_LETTER(6) == 'G');
+}
+
+TEST_CASE("Columns to letters", "[defines]")
+{
+    REQUIRE(COL_TO_LETTER(0) == 'a');
+    REQUIRE(COL_TO_LETTER(1) == 'b');
+    REQUIRE(COL_TO_LETTER(2) == 'c');
+    REQUIRE(COL_TO_LETTER(3) == 'd');
+    REQUIRE(COL_TO_LETTER(4) == 'e');
+    REQUIRE(COL_TO_LETTER(5) == 'f');
+    REQUIRE(COL_TO_LETTER(6) == 'g');
+}
+
+TEST_CASE("Letters to rows", "[defines]")
+{
+    REQUIRE(LETTER_TO_ROW('A') == 0);
+    REQUIRE(LETTER_TO_ROW('B') == 1);
+    REQUIRE(LETTER_TO_ROW('C') == 2);
+    REQUIRE(LETTER_TO_ROW('D') == 3);
+    REQUIRE(LETTER_TO_ROW('E') == 4);
+    REQUIRE(LETTER_TO_ROW('F') == 5);
+    REQUIRE(LETTER_TO_ROW('G') == 6);
+}
+
+TEST_CASE("Letters to columns", "[defines]")
+{
+    REQUIRE(LETTER_TO_COL('a') == 0);
+    REQUIRE(LETTER_TO_COL('b') == 1);
+    REQUIRE(LETTER_TO_COL('c') == 2);
+    REQUIRE(LETTER_TO_COL('d') == 3);
+    REQUIRE(LETTER_TO_COL('e') == 4);
+    REQUIRE(LETTER_TO_COL('f') == 5);
+    REQUIRE(LETTER_TO_COL('g') == 6);
 }
